99ddc17250b8bdd3c46f53513d60a18edd7e4138
irem-koc <irem.koc1@std.yildiz.edu.tr>
2024-10-10 00:36:01 +0300

    Refactor layout to include Providers component for managing query client in app

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-
+import Providers from "./providers";
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
@@ -25,8 +25,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+      <body
+        className={`${geistSans.variable} ${geistMono.variable}`}
+        suppressHydrationWarning={true}
+      >
+        <Providers>{children}</Providers>
       </body>
     </html>
   );

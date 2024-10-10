16eecbfcb1eb7f13775045a07528c0a0ebadc620
irem-koc <irem.koc1@std.yildiz.edu.tr>
2024-10-11 00:13:00 +0300

    Refactor Nav component styles to add logo image and search input

--- components/Nav/Nav.tsx
+++ components/Nav/Nav.tsx
@@ -3,10 +3,13 @@ import style from "./style.module.css";
 const Nav = () => {
   return (
     <header className={style.container}>
-      <div>
-        <Image alt="logo" src={"/m"} width={50} height={50} />
-        <h1 className={style.header}>Hello</h1>
+      <div className={style.logoContainer}>
+        <Image alt="logo" src={"/logo-food.webp"} width={100} height={100} />
+        <h1 className={style.header}>... Company</h1>
       </div>
+      <form>
+        <input type="text" placeholder="Aradığım ürün..." />
+      </form>
     </header>
   );
 };
